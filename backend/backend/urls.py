f4d4741642e5e8dcd06ed8d5bc30f70993e5052a
Wilton <129124024+Wiltonll@users.noreply.github.com>
2025-04-04 12:55:38 -0300

    add root endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def root(request):
+    return JsonResponse({"message": "Hello, Django API is running 🚀"})
 
 urlpatterns = [
+    path('', root),
     path('admin/', admin.site.urls),
     path('api/products/',include('api.urls.product_urls')),
     path('api/users/',include('api.urls.user_urls')),
